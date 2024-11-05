c79cd66ebb86fd248411b5642b9bd5ff265795e3
Maxim Degtiarev <mxim.degt@gmail.com>
2024-11-05 11:08:57 -0300

    Image saving and size limit controller

--- clase-24/frontend/src/Screens/Home/Home.jsx
+++ clase-24/frontend/src/Screens/Home/Home.jsx
@@ -8,18 +8,6 @@ const Home = () => {
     const user_info = JSON.parse(sessionStorage.getItem('user_info'));
     const { products } = useProducts();
 
-    // useEffect(() => {
-    //     const fetchProducts = async () => {
-    //         try {
-    //             const productsToGet = await GET('http://127.0.0.1:3000/api/products');
-    //             setProducts(productsToGet.payload.products);
-    //         } catch (err) {
-    //             console.error(err);
-    //         }
-    //     };
-    //     fetchProducts();
-    // }, []);
-
     return (
         <div>
             <h1>
